28b4c8a2b24093458030fbca197fe348ecfb09b1
Moon <sinbadra@gmail.com>
2020-03-20 14:16:57 +0900

    입력 조건은 0~1이므로 중간에 답이 더 작아질 경우 최대값이 나올 수 없다.

--- samsung/D4/1865_동철이의 일 분배.py
+++ samsung/D4/1865_동철이의 일 분배.py
@@ -1,33 +1,35 @@
 # def cal(n, S, O):
 def cal(n, S):
     global P, ans, arr
+    if S <= ans:
+        return
+        
     if n == N:
         sol = S
-        # print(O)
         if S > ans:
             ans = sol
         return
 
     for i in range(N):
-        if arr[i]:
-            continue
-        else:
-            arr[i] = 1
-            # cal(n + 1, S * P[n][i] * 0.01, O + [P[n][i]])
-            cal(n + 1, S * P[n][i] * 0.01)
-            arr[i] = 0        
+        if not arr[i]:
+            if P[n][i] == 0:
+                continue
+            else:
+                arr[i] = 1
+                cal(n + 1, S * P[n][i] * 0.01)
+                arr[i] = 0        
 
 
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
     P = [list(map(int, input().split())) for _ in range(N)]
+    print(P)
     ans = 0
     arr = [0] * N
     for i in range(N):
         arr[i] = 1
-        # cal(1, P[0][i] * 0.01, [P[0][i]])
         cal(1, P[0][i] * 0.01)
         arr[i] = 0
         
-    print("#{} {}".format(test_case, round(ans * 100, 6)))
+    print("#{} {:0.6f}".format(test_case, round(ans * 100, 6)))
