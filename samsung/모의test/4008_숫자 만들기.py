af87cccb3023d1b557b7cecd99609d5e845213ef
Moon <sinbadra@gmail.com>
2020-04-23 17:23:12 +0900

    solved. cal 함수 삭제, dict 삭제.

--- samsung/모의test/4008_숫자 만들기.py
+++ samsung/모의test/4008_숫자 만들기.py
@@ -1,15 +1,5 @@
-def cal(op, A, B):
-    if op == "+":
-        return A + B
-    elif op == "-":
-        return A - B
-    elif op == "*":
-        return A * B
-    elif op == "/":
-        return int(A / B)
-
 def dfs(i,tmp_sum):
-    global N, ans_max, ans_min, OP, operator, nums
+    global N, ans_max, ans_min, nums, num_op
     if i == N:
         if tmp_sum > ans_max:
             ans_max = tmp_sum
@@ -17,21 +7,26 @@ def dfs(i,tmp_sum):
             ans_min = tmp_sum
         return
 
-    for op in operator:
-        if OP[op] > 0:
-            OP[op] -= 1
-            dfs(i+1, cal(op, tmp_sum, nums[i]))
-            OP[op] += 1
+    for j in range(4):
+        if num_op[j] > 0:
+            num_op[j] -= 1
+            if j == 0:
+                tmp = tmp_sum + nums[i]
+            elif j == 1:
+                tmp = tmp_sum - nums[i]
+            elif j == 2:
+                tmp = tmp_sum * nums[i]
+            elif j == 3:
+                tmp = int(tmp_sum / nums[i])
+            dfs(i+1, tmp)
+            num_op[j] += 1
 
 T = int(input())
 for test_case in range(1, T + 1):
     N =int(input())
-    operator = ["+", "-", "*", "/"]
     num_op = list(map(int, input().split()))
     nums = list(map(int, input().split()))
-    OP = dict(zip(operator, num_op))
-    ans_max = float('-inf')
-    ans_min = float('inf')
+    ans_max, ans_min = float('-inf'), float('inf')
     dfs(1, nums[0])
     ans = ans_max - ans_min
     print("#{} {}".format(test_case, ans))
